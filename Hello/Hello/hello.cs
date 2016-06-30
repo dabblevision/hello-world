359c73a18ba3c39264c4c9a62e74910361e68e67
dabblevision <darian.sanatpour@gmail.com>
2016-06-30 15:34:32 +0200

    Finished the first version of the Hello Project

--- Hello/Hello/hello.cs
+++ Hello/Hello/hello.cs
@@ -1,10 +1,41 @@
 using System;
 
-public class Program
+namespace Hello
 {
-  static void Main()
-  {
-    Console.WriteLine("Hello, World!");
-    Console.WriteLine(DateTime.Now.DayOfWeek);
-  }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+                Console.WriteLine("Hello, " + name + "!");
+            }
+            else
+            {
+                Console.WriteLine("Hello, " + args[1] + "!");
+            }
+            Console.WriteLine("Today is a " + DateTime.Now.DayOfWeek + ".");
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep;
+            if (int.TryParse(Console.ReadLine(), out hoursOfSleep))
+            {
+                if (hoursOfSleep >= 8)
+                {
+                    Console.WriteLine("Your are well rested."); 
+                }
+                else
+                {
+                    Console.WriteLine("You are not well rested");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Couldn't parse your input as an integer.");
+                Console.WriteLine("hoursOfSleep variable holds the value: " + hoursOfSleep);
+            }
+            Console.ReadLine();
+        }
+    }
 }
